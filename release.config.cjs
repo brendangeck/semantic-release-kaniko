45f885b2cc5114e9513b53851de8067c8cf7fe1f
brendangeck <bpgeck@gmail.com>
2024-08-12 16:50:42 -0700

    fix: Updating our release rules to work with our prefix convention

--- release.config.cjs
+++ release.config.cjs
@@ -2,8 +2,22 @@ module.exports = {
     branches: ['main'],
     plugins: [
         '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/changelog',
         '@semantic-release/npm',
         '@semantic-release/github',
         '@semantic-release/git',
     ],
+    releaseRules: [
+        { type: 'breaking', release: 'major' },
+        { type: 'feat', release: 'minor' },
+        { type: 'fix', release: 'patch' },
+        { type: 'perf', release: 'minor' },
+        { type: 'docs', release: false },
+        { type: 'chore', release: false },
+        { type: 'style', release: false },
+        { type: 'refactor', release: false },
+        { type: 'test', release: false },
+    ],
+    preset: 'angular',
 };
