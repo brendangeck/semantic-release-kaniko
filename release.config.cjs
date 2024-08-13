1e164c7619ca17c846b054029bea7bad9991742d
Brendan Geck <71093409+brendangeck@users.noreply.github.com>
2024-08-13 13:50:29 -0700

    fix: Removing changelog and git plugins to follow semantic release guidance (#9)
    * fix: Removing the changelog and git plugins for semantic release
    
    * Adding more guidance on how to use the plugin
    
    * Removing version number to reduce confusion in deployment process
    
    * Unifying workflows into a single file

--- release.config.cjs
+++ release.config.cjs
@@ -3,10 +3,8 @@ module.exports = {
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
-        '@semantic-release/changelog',
         '@semantic-release/npm',
         '@semantic-release/github',
-        '@semantic-release/git',
     ],
     releaseRules: [
         { type: 'breaking', release: 'major' },
