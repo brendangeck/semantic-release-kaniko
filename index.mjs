521588a57d5bcc37bcb735565d1674cee666f6eb
brendangeck <bpgeck@gmail.com>
2024-08-13 15:29:37 -0700

    fix: Removing the mixure of CommonJS and ES import syntax

--- index.mjs
+++ index.mjs
@@ -2,4 +2,4 @@ import { verifyConditions } from './lib/verifyConditions.mjs';
 import { prepare } from './lib/prepare.mjs';
 import { publish } from './lib/publish.mjs';
 
-module.exports = { verifyConditions, prepare, publish };
+export { verifyConditions, prepare, publish };
